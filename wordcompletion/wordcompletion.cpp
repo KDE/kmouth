dc5868ee36d3b4aad8de0a940133bc3974eceae0
Albert Astals Cid <tsdgeos@terra.es>
2009-02-01 19:55:40 +0000

    revert parts of r918686, commit by annma from a patch by Thomas Richard
    It changes licenses from GPLv2 to GPLv2+ and adds license headers to some files.
    
    I'm reverting this until we find out if Thomas contacted the proper authors and they agreed or if it was just a newbie mistake
    
    
    svn path=/trunk/KDE/kdeaccessibility/kmouth/; revision=919889

--- wordcompletion/wordcompletion.cpp
+++ wordcompletion/wordcompletion.cpp
@@ -1,20 +1,3 @@
-/***************************************************************************
-                          wordcompletion.cpp  -  description
-                             -------------------
-    begin                : Tue Apr 29 2003
-    copyright            : (C) 2002 by Gunnar Schmi Dt
-    email                : kmouth@schmi-dt.de
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
 #include "wordcompletion.h"
 
 #include <QtCore/QRegExp>
