0c68d601dfa4a6858fb418799eae1763c6d8841a
Jeremy Whiting <jpwhiting@kde.org>
2015-04-30 19:31:02 -0600

    Fix usage of KIconLoader in phraselist to get speak icon.

--- phraselist.cpp
+++ phraselist.cpp
@@ -81,7 +81,7 @@ PhraseList::PhraseList(QWidget *parent, const char *name) : QWidget(parent) {
    rowLayout->addWidget(lineEdit);
    lineEdit->setFocus();
 
-   QIcon icon = KIconLoader::global()->loadIconSet(QLatin1String( "speak" ), KIconLoader::Small);
+   QIcon icon = KIcon(QLatin1String( "speak" ));
    speakButton = new QPushButton (icon, i18n("&Speak"), this);
    speakButton->setFocusPolicy(Qt::NoFocus);
    speakButton->setAutoDefault(false);
